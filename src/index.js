c4c9804f04458ca3190b11669ac6f3db416e8b81
Nicholas Leask <leask.nicholas@gmail.com>
2018-06-26 11:56:30 -0400

    UI layout in place

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ fetch(myAPI)
       });
 
 function initMap() {
-  var myLatLng = {lat: -25.363, lng: 131.044};
+  var myLatLng = {lat: 40.726002, lng: -74.008246};
 
   var map = new google.maps.Map(document.getElementById('map'), {
     center: myLatLng,
-    zoom: 4
+    zoom: 20
   });
 
   var marker = new google.maps.Marker({
