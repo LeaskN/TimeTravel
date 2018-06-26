14d05125e21787791f1234473b1134452b88382c
Nicholas Leask <leask.nicholas@gmail.com>
2018-06-26 16:30:39 -0400

    Location Search Working

--- src/index.js
+++ src/index.js
@@ -1,25 +1,65 @@
 import 'bootswatch/dist/pulse/bootstrap.css';
 import './styles.css';
-const corsAPI = "https://galvanize-cors.herokuapp.com/"
-var myAPI = corsAPI + "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&key=AIzaSyDw8zDFkLcYlC67ewWhYGOm1irmClp2s1c";
 
-console.log('Dumbledore');
+const corsAPI = "https://galvanize-cors.herokuapp.com/";
+const myAPI = corsAPI + `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=laundry&radius=1500&key=AIzaSyDw8zDFkLcYlC67ewWhYGOm1irmClp2s1c`;
+const locationInput = document.querySelector('#inputSearch');
+const form = document.querySelector(".form");
+const userLocationInput = document.getElementById("inputSearch");
 
-fetch(myAPI)
+let latitude = 51.5074;
+let longitude = 0.1278;
+let myNewAPI = corsAPI + `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=laundry&radius=1500&key=AIzaSyDw8zDFkLcYlC67ewWhYGOm1irmClp2s1c`;
+
+document.addEventListener("DOMContentLoaded", getLocation);
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("Geolocation is not supported by this browser.")
+    }
+}
+
+function showPosition(position) {
+    latitude = position.coords.latitude
+    longitude = position.coords.longitude
+    myNewAPI = corsAPI + `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=laundry&radius=1500&key=AIzaSyDw8zDFkLcYlC67ewWhYGOm1irmClp2s1c`;
+    initMap(myAPI);
+    fetch(myNewAPI)
       .then(response => response.json())
-      .then(results => {
-          console.log(results);
+      .then(data => {
+          let responses = data.results;
+          // console.log(`${latitude},${longitude}`);
+          responses.forEach(function(element){
+            // console.log(element.name);
+          })
       });
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const searchLocation = userLocationInput.value;
+  // console.log(searchLocation)//object(of lat and long).value
+  // console.log('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&key=AIzaSyDw8zDFkLcYlC67ewWhYGOm1irmClp2s1c');
+  const myNewAPI = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchLocation}&key=AIzaSyDw8zDFkLcYlC67ewWhYGOm1irmClp2s1c`;
+  fetch(myNewAPI)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.results["0"].geometry.location.lat);
+      console.log(data.results["0"].geometry.location.lng);
+  });
+});
 
 function initMap() {
-  var myLatLng = {lat: 40.726002, lng: -74.008246};
+  let myLatLng = {lat: latitude, lng: longitude};
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+  let map = new google.maps.Map(document.getElementById('map'), {
     center: myLatLng,
-    zoom: 20
+    zoom: 8
   });
 
-  var marker = new google.maps.Marker({
+  let marker = new google.maps.Marker({
     map: map,
     position: myLatLng,
     title: 'Hello World!'
@@ -35,8 +75,8 @@ ratingOutput.innerHTML = ratingSlider.value + "/10";
 ratingSlider.oninput = function() {
   ratingOutput.innerHTML = this.value + "/10";
 }
-var distanceSlider = document.getElementById("myDistance");
-var distanceOutput = document.getElementById("distanceNumber");
+let distanceSlider = document.getElementById("myDistance");
+let distanceOutput = document.getElementById("distanceNumber");
 distanceOutput.innerHTML = distanceSlider.value + "km";
 
 distanceSlider.oninput = function() {
